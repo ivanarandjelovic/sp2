06949f0864a50622993e8451a9bc575ef7ce8f33
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2017-12-21 17:46:59 +0100

    Change annotation to springbootapp

--- src/main/java/org/aivan/sp2/Sp2Application.java
+++ src/main/java/org/aivan/sp2/Sp2Application.java
@@ -1,11 +1,11 @@
 package org.aivan.sp2;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 @RestController
 public class Sp2Application {
 	public static void main(String[] args) {
